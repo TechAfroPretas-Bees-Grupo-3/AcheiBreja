1a22edd842ca8ef150bc8506e534334e79c394a2
Daiane Goncalves <daigoncalves1409@gmail.com>
2023-05-06 00:54:38 -0300

    Criação do Projeto AcheiBreja

---
+++ achei-breja/src/main/java/tech/afro/pretas/acheibreja/AcheiBrejaApplication.java
@@ -0,0 +1,13 @@
+package tech.afro.pretas.acheibreja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcheiBrejaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcheiBrejaApplication.class, args);
+	}
+
+}
---
+++ achei-breja/src/test/java/tech/afro/pretas/acheibreja/AcheiBrejaApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.afro.pretas.acheibreja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcheiBrejaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

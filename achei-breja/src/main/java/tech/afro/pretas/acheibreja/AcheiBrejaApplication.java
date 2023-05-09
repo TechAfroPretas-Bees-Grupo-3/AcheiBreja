b17892676842a553772a1d03bd27c24ef0371c1f
Daiane Goncalves <daigoncalves1409@gmail.com>
2023-05-09 18:37:33 -0300

    Limpeza no application e ManyToOne requisicao

--- achei-breja/src/main/java/tech/afro/pretas/acheibreja/AcheiBrejaApplication.java
+++ achei-breja/src/main/java/tech/afro/pretas/acheibreja/AcheiBrejaApplication.java
@@ -1,56 +1,16 @@
 package tech.afro.pretas.acheibreja;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import tech.afro.pretas.acheibreja.repository.CategoriaRepository;
-import tech.afro.pretas.acheibreja.repository.EstabelecimentoRepository;
-import tech.afro.pretas.acheibreja.repository.ProdutoRepository;
 
 @SpringBootApplication
-public class AcheiBrejaApplication implements CommandLineRunner {
+public class AcheiBrejaApplication {
 
-	@Autowired // metodo spring para instanciar o objeto
-	private CategoriaRepository categoriaRepository;
 
-	@Autowired
-	private ProdutoRepository produtoRepository;
-
-	@Autowired
-	private EstabelecimentoRepository EstabelecimentoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AcheiBrejaApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-//		Categoria a = new Categoria();
-//		a.setIdCategoria("C1");
-//		a.setTipoCategoria("IPA");
-//		categoriaRepository.save(a);
-//		
-//		Categoria b = categoriaRepository.findById("C1").get();
-//		System.out.println(b.getTipoCategoria());
-//
-//		Endereco e = new Endereco();
-//		e.setIdEndereco((long) 1);
-//		e.setLogradouro("rua da cerveja");
-//		e.setBairro("taquaral");
-//		enderecoRepository.save(e);
-//
-//		Produto p = new Produto();
-//		p.setId("P1");
-//		p.setNome("Becks");
-//		p.setPreco(4.5);
-//		p.setCategoria(a);
-//
-//		Set<Endereco> listaEndereco = new HashSet<>();
-//		listaEndereco.add(e);
-//		p.setListaEndereco(listaEndereco);
-//		produtoRepository.save(p);
-		
-	}
 
 }
--- achei-breja/src/main/java/tech/afro/pretas/acheibreja/model/Requisicao.java
+++ achei-breja/src/main/java/tech/afro/pretas/acheibreja/model/Requisicao.java
@@ -1,7 +1,6 @@
 package tech.afro.pretas.acheibreja.model;
 
-import java.sql.Date;
-
+import java.util.Calendar;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
@@ -17,26 +16,61 @@ public class Requisicao {
 	@Column(name = "id_requisicao")
 	private Long idRequisicao;
 	
-//	@ManyToOne
-//	@JoinColumn(name="cart_id", nullable=false)
-//	@Column(name = "id_usuario")
-//	private Long idUsuario;
-////	
-//	@ManyToOne
-//	@JoinColumn(name = "id_categoria")
-//	private Categoria categoria;
+	@ManyToOne
+	@JoinColumn(name = "id_usuario")
+	private Usuario usuario;
+
+	@ManyToOne
+	@JoinColumn(name = "id_produto")
+	private Produto produto;
 	
 	@Column(name = "id_quantidade_requisicao")
 	private Long idQuantidadeRequisicao;
 	
 	@Column(name = "data_requisicao")
-	private Date dataRequisicao;
+	private Calendar dataRequisicao;
 
 	public Requisicao() {
 		super();
 	}
-	
-	
-	
-	
+
+	public Long getIdRequisicao() {
+		return idRequisicao;
+	}
+
+	public void setIdRequisicao(Long idRequisicao) {
+		this.idRequisicao = idRequisicao;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	public Produto getProduto() {
+		return produto;
+	}
+
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+
+	public Long getIdQuantidadeRequisicao() {
+		return idQuantidadeRequisicao;
+	}
+
+	public void setIdQuantidadeRequisicao(Long idQuantidadeRequisicao) {
+		this.idQuantidadeRequisicao = idQuantidadeRequisicao;
+	}
+
+	public Calendar getDataRequisicao() {
+		return dataRequisicao;
+	}
+
+	public void setDataRequisicao(Calendar dataRequisicao) {
+		this.dataRequisicao = dataRequisicao;
+	}
 }
